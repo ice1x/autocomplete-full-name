de0ab5548a7b2419d7c6248679fd001f76a60e28
ice1x <ice1x@gmail.com>
2020-11-08 01:38:39 +0500

    Remove optional parameters

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from prefix_trie import client
 app = FastAPI()
 
 
-@app.get("/suggest/{query}&{count}")
-async def suggest(query: str, count: int):
+@app.get("/suggest/")
+async def suggest(query: str, count: int = 10):
     return client.suggest(query, count)
 
 
-@app.get("/parse/{query}")
+@app.get("/parse/")
 async def parse(query: str):
     return client.parse(query)
