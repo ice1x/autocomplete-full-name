4d23689ce0437f2c703b681e744379ac3e6fe368
cold00n <ilya.yahin@factory5.ai>
2021-05-14 23:26:49 +0500

    Fix client test

--- fastapi_test.py
+++ fastapi_test.py
@@ -3,23 +3,24 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+EXPECTED = [
+    {'name': 'александр', 'amount': '135746', 'gender': 'male', 'type': 'firstname'},
+    {'name': 'александрович', 'amount': '129599', 'gender': 'male', 'type': 'middlename'},
+    {'name': 'александровна', 'amount': '86711', 'gender': 'female', 'type': 'middlename'},
+    {'name': 'андрей', 'amount': '84723', 'gender': 'male', 'type': 'firstname'},
+    {'name': 'алексей', 'amount': '82645', 'gender': 'male', 'type': 'firstname'},
+    {'name': 'анатольевич', 'amount': '68638', 'gender': 'male', 'type': 'middlename'},
+    {'name': 'анатольевна', 'amount': '45446', 'gender': 'female', 'type': 'middlename'},
+    {'name': 'алексеевич', 'amount': '42811', 'gender': 'male', 'type': 'middlename'},
+    {'name': 'анна', 'amount': '30225', 'gender': 'female', 'type': 'firstname'},
+    {'name': 'алексеевна', 'amount': '29105', 'gender': 'female', 'type': 'middlename'}
+]
 
 
 def test_read_main():
-    response = client.get("/suggest/%D0%B0&10")
+    response = client.get("/suggest/?query=%D0%B0&10")
     assert response.status_code == 200
-    assert response.json() == [
-        {"name": "амбарцумович", "amount": "99", "gender": "male", "type": "middlename"},
-        {"name": "адгамович", "amount": "99", "gender": "male", "type": "middlename"},
-        {"name": "авилов", "amount": "99", "gender": "male", "type": "lastname"},
-        {"name": "аминова", "amount": "98", "gender": "female", "type": "lastname"},
-        {"name": "алтухова", "amount": "98", "gender": "female", "type": "lastname"},
-        {"name": "айшат", "amount": "98", "gender": "female", "type": "firstname"},
-        {"name": "архипенко", "amount": "97", "gender": None, "type": "lastname"},
-        {"name": "арапова", "amount": "97", "gender": "female", "type": "lastname"},
-        {"name": "акчурина", "amount": "97", "gender": "female", "type": "lastname"},
-        {"name": "арсеньева", "amount": "96", "gender": "female", "type": "lastname"}
-    ]
+    assert response.json() == EXPECTED
 
 
 if __name__ == "__main__":
